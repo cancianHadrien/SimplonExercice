73950551119b512fb030762da14b0aa9d6d31b81
hcancian <cancianhadrien@gmail.com>
2020-09-16 14:31:31 +0200

    partie3

--- ProjetNetflix/NetflixP3.py
+++ ProjetNetflix/NetflixP3.py
@@ -6,14 +6,7 @@ Created on Tue Sep 15 09:38:58 2020
 @author: simplon
 """
 import pandas as pd
-import sqlalchemy as db
-import re
-import numpy as np
-
-engine = db.create_engine('mysql+pymysql://hadrien:H01011997C@localhost:3306/Netflix')
-connection = engine.connect()
-
-donnees = pd.read_csv('/home/simplon/Téléchargements/NETFLIX/netflix_titles.csv')
+donnees = pd.read_csv('/home/simplon/Téléchargements/Netflix/netflix_titles.csv')
 #print(donnees)
 
 #EXO20
@@ -121,56 +114,125 @@ donnees = pd.read_csv('/home/simplon/Téléchargements/NETFLIX/netflix_titles.cs
 
 #EXO 23
 
+from sqlalchemy import create_engine
+
+df = pd.read_csv("/home/simplon/Téléchargements/Netflix/netflix_titles.csv")
+engine = create_engine('mysql+pymysql://nouveau_utilisateur:mot_de_passe@localhost:3306/netflix')
+
+#df=id/categorie
+cat_name = pd.DataFrame(data = df,columns = ["ca_id", "cat_name"])
+cat_name = cat_name.dropna(inplace=True)
 
 
-engine.execute("SET FOREIGN_KEY_CHECKS=0;")
-engine.execute("TRUNCATE catalog;")
-engine.execute("TRUNCATE catalog_country;")
-engine.execute("TRUNCATE Country;")
-engine.execute("TRUNCATE catalog_category;")
-engine.execute("TRUNCATE category;")
-engine.execute("TRUNCATE catalog_actor;")
-engine.execute("TRUNCATE actor;")
-engine.execute("TRUNCATE catalog_director;")
-engine.execute("TRUNCATE director;")
-engine.execute("SET FOREIGN_KEY_CHECKS=1;")
+#df=id/cast
+cast = pd.DataFrame(data = df,columns = ["ca_id", "cast_name"])
 
-def getTableData(colFilter, valNameDefault, newValName, colNameID, insertIntoTblNames):
-    # Intialisation des variables
-    dataFramesLst = []
-    data_filtered = donnees[colFilter]
-    data_filtered = donnees.loc[donnees[valNameDefault].notna()][colFilter]
-    colNameFirst, colNameSecond = [], []
-    # On parcour toutes les lignes
-    for index, row in data_filtered.iterrows():
-        for subdata in row[1].split(", "):
-            if not re.search(r',$', subdata):
-                colNameFirst.append(row[0])
-                colNameSecond.append(subdata)
-    # Premier DataFrame
-    firstDFtmp = pd.DataFrame({colFilter[0]: colNameFirst, colFilter[1]: colNameSecond}, columns = colFilter)
-    firstDFtmp = firstDFtmp.rename(columns={valNameDefault:newValName, 'show_id':'catalog_id'})
-    firstDF = firstDFtmp.drop(columns='catalog_id').drop_duplicates()
-    firstDF.index = np.arange(1,len(firstDF)+1)
-    # Deuxieme DataFrame
-    secondDF = firstDFtmp[newValName].drop_duplicates().reset_index(drop=True).reset_index().rename(columns={'index':colNameID})
-    secondDF[colNameID] = np.arange(1,len(secondDF)+1)
-    secondDF = secondDF.merge(firstDFtmp, on=newValName, how='left').drop(columns=[newValName])
-    # Ajout des données dans les 2 tables
-    firstDF.to_sql(insertIntoTblNames[0], con=engine, index = False, if_exists = 'append')
-    secondDF.drop_duplicates().to_sql(insertIntoTblNames[1], con=engine, index = False, if_exists = 'append')
+cast = cast.dropna(inplace=True)
+print(cast)
+
+#df=id/director
+directors = pd.DataFrame(data = df,columns = ["ca_id", "dir_name"])
+directors.dropna(inplace=True)
+
+#df=id/country
+country = pd.DataFrame(data = df,columns = ["ca_id", "co_name"])
+country.dropna(inplace=True)
+
+#fonction split 
+def separator(id, column, column_name):
+    c = 0
+    liste_id = []
+    liste_col = []
+    for i in column:
+        x = i.split(", ")
+        for z in x:
+            liste_col.append(z)
+            liste_id.append(id[c])
+        
+        c+=1
+    dataframe = pd.DataFrame({'ca_id':liste_id, column_name: liste_col}, columns = ['ca_id', column_name])
+    return dataframe 
 
 
-# Catalog
-data_catalog = donnees.drop(columns=['listed_in', 'country', 'cast', 'director']).rename(columns={'show_id':'catalog_show_id', 'type':'catalog_type', 'title':'catalog_title', 'date_added':'catalog_date_added', 'rating':'catalog_rating', 'release_year':'catalog_release_year', 'duration':'catalog_duration', 'description':'catalog_description'})
-data_catalog["catalog_date_added"] = pd.to_datetime(donnees["date_added"])
-data_catalog = data_catalog.replace({'catalog_duration' : { ' min' : '', ' Seasons' : '', ' Season' : '' }}, regex=True)
-data_catalog.to_sql('catalog', con=engine, index = False, if_exists = 'append')
-# Category
-category = getTableData(["show_id", "listed_in"], "listed_in", "category_name", "category_id", ["category", "catalog_category"])
-# Country
-country = getTableData(["show_id", "country"], "country", "country_name", "country_id", ["country", "catalog_country"])
-# Actor
-actor = getTableData(["show_id", "cast"], "cast", "actor_name", "actor_id", ["actor", "catalog_actor"])
-# Director
-director = getTableData(["show_id", "director"], "director", "director_name", "director_id", ["director", "catalog_director"])
+
+
+
+#dataframe id/catégories
+cat_id=separator(cat_name['ca_id'], cat_name['cat_name'], 'cat_name')
+#dataframe id/cast
+cast_id=separator(cat_name['ca_id'], cast['cast_name'], 'cast_name')
+#dataframe id/directors
+dir_id=separator(cat_name['ca_id'], directors['dir_name'], 'dir_name')
+#dataframe id/country
+country_id=separator(cat_name['ca_id'], country['co_name'], 'co_name')
+
+
+def table(df, column ,id): #formatage table
+    data = df.drop_duplicates(subset=[column]).reset_index().rename(columns={"index": id})
+    del data['ca_id']
+    print(data)
+    return data
+
+def associationTable(df_base, df, column ,id): #formatage table association
+    data= df.merge(df_base, left_on=column, right_on= column)
+    data = data.rename(columns={"ca_id_x": "ca_id"})
+    del data[column]
+    print(data)
+    return data
+
+
+#table categorie catalogue
+categories = table(cat_id, "cat_name", "cat_id")
+cat_categories = associationTable(categories, cat_id, "cat_name", "cat_id")
+
+#table country catalogue
+country = table(country_id, "co_name", "co_id")
+cat_country = associationTable(country, country_id, "co_name", "co_id")
+
+#table cast catalogue
+cast = table(cast_id, "cast_name", "cast_id")
+cat_cast = associationTable(cast, cast_id, "cast_name", "cast_id")
+
+#table dir catalogue
+
+dir = table(dir_id, "dir_name", "dir_id")
+cat_dir = associationTable(dir, dir_id, "dir_name", "dir_id")
+
+
+#table catalogue
+del df['cat_name']
+del df['dir_name']
+del df['cast_name']
+del df['co_name']
+del df['Unnamed: 12']
+
+
+
+#changement type durée et date ajout
+
+df['ca_duration'] = df['ca_duration'].str.replace(" min", "").str.replace(" Season", "").str.replace(" season", "").str.replace("s", "").astype(int)
+df['ca_date']=pd.to_datetime(df['ca_date'])
+
+
+
+#insertion tables
+
+df.to_sql('catalogue', con=engine, if_exists='append', index=False)
+country.to_sql('country', con=engine, if_exists='append', index=False)
+cast.to_sql('cast', con=engine, if_exists='append', index=False)
+dir.to_sql('directors', con=engine, if_exists='append', index=False)
+
+
+
+#insertion tables d'association
+
+cat_country.to_sql('catalogue_country', con=engine, if_exists='append', index=False)
+cat_cast.to_sql('catalogue_cast', con=engine, if_exists='append', index=False)
+cat_dir.to_sql('catalogue_directors', con=engine, if_exists='append', index=False)
+cat_categories.to_sql('catalogue_category', con=engine, if_exists='append', index=False)
+
+
+
+
+
+
